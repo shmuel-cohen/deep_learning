3fe59da5ef03b1a4136c07bf7847c6b95d263ecb
itanini <itamar.nini@mail.huji.ac.il>
2024-06-23 15:33:44 +0300

    base modek

--- ex2/Autoencoder.py
+++ ex2/Autoencoder.py
@@ -10,21 +10,21 @@ class Autoencoder(nn.Module):
     def __init__(self):
         super(Autoencoder, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(16, 8, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(16, 4, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(8, 16,
+            nn.ConvTranspose2d(4, 16,
                                kernel_size=3,
                                stride=2,
                                padding=1,
                                output_padding=1),
             nn.ReLU(),
-            nn.ConvTranspose2d(16, 3,
+            nn.ConvTranspose2d(16, 1,
                                kernel_size=3,
                                stride=2,
                                padding=1,
