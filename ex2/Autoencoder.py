d3f910a5e337b41717d6965c5e5f90aac54376f9
shmuel <3820012>
2024-06-23 14:56:17 +0300

    copy model

---
+++ ex2/Autoencoder.py
@@ -0,0 +1,80 @@
+import torch
+from torchvision import datasets
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
+# Transforms images to a PyTorch Tensor
+tensor_transform = transforms.ToTensor()
+
+# Download the MNIST Dataset
+dataset = datasets.MNIST(root="./data",
+                         train=True,
+                         download=True,
+                         transform=tensor_transform)
+
+# DataLoader is used to load the dataset
+# for training
+loader = torch.utils.data.DataLoader(dataset=dataset,
+                                     batch_size=32,
+                                     shuffle=True)
+
+
+# Creating a PyTorch class
+# 28*28 ==> 9 ==> 28*28
+class AE(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # Building an linear encoder with Linear
+        # layer followed by Relu activation function
+        # 784 ==> 9
+        self.encoder = torch.nn.Sequential(
+            torch.nn.Linear(28 * 28, 128),
+            torch.nn.ReLU(),
+            torch.nn.Linear(128, 64),
+            torch.nn.ReLU(),
+            torch.nn.Linear(64, 36),
+            torch.nn.ReLU(),
+            torch.nn.Linear(36, 18),
+            torch.nn.ReLU(),
+            torch.nn.Linear(18, 9)
+        )
+
+        # Building an linear decoder with Linear
+        # layer followed by Relu activation function
+        # The Sigmoid activation function
+        # outputs the value between 0 and 1
+        # 9 ==> 784
+        self.decoder = torch.nn.Sequential(
+            torch.nn.Linear(9, 18),
+            torch.nn.ReLU(),
+            torch.nn.Linear(18, 36),
+            torch.nn.ReLU(),
+            torch.nn.Linear(36, 64),
+            torch.nn.ReLU(),
+            torch.nn.Linear(64, 128),
+            torch.nn.ReLU(),
+            torch.nn.Linear(128, 28 * 28),
+            torch.nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
+
+def main():
+    # Model Initialization
+    model = AE()
+
+    # Validation using MSE Loss function
+    loss_function = torch.nn.MSELoss()
+
+    # Using an Adam Optimizer with lr = 0.1
+    optimizer = torch.optim.Adam(model.parameters(),
+                                 lr=1e-1,
+                                 weight_decay=1e-8)
+
+
+if __name__ == '__main__':
+    main()
