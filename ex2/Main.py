8d381b4f98fba659b94ae7920df54893f4a34fd2
shmuel <3820012>
2024-06-28 02:36:05 +0300

    complete Q4. Q5 need for saved model. fix Q1

--- ex2/Main.py
+++ ex2/Main.py
@@ -13,8 +13,9 @@ import Classifier
 import Pretrained_encoder
 import numpy as np
 
-NUM_EPOCHS = 7
+NUM_EPOCHS = 30
 BATCH_SIZE = 256
+PATH_TO_SAVED_AE = ""
 def data_loader():
     global train_loader, test_loader
     BATCH_SIZE = 256
@@ -26,6 +27,79 @@ def data_loader():
     train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
     return train_loader, test_loader, test_dataset
+
+def Q1_S(train_loader, test_loader):
+    model = Autoencoder.Autoencoder()
+    criterion = nn.L1Loss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    train_loss, test_loss = [],[]
+    for epoch in range(NUM_EPOCHS):
+        model.train() #why do you take it out from the next loop? we dont want train on every batch?
+        agg_train_loss = 0
+        correct_train = 0
+        total_train = 0
+
+        for data_batch, _ in train_loader:
+            # data_batch, label_batch = data_batch.to(model.device), label_batch.to(model.device)
+
+            # one_hot_label_batch = one_hot_encode(label_batch)
+            optimizer.zero_grad()
+            output = model(data_batch)
+            loss = criterion(output, data_batch)
+            loss.backward()
+            optimizer.step()
+            agg_train_loss += loss.item()
+
+            _, predicted = torch.max(output, 1)
+            correct_train += (predicted == data_batch).sum().item()
+            total_train += data_batch.size(0)
+
+        train_loss.append(agg_train_loss / len(train_loader))
+        # train_accuracy.append(100 * correct_train / total_train)
+
+
+        model.eval()
+        agg_test_loss = 0
+        correct_test = 0
+        total_test = 0
+
+        with torch.no_grad():
+            for data_batch, label_batch in test_loader:
+                # data_batch, label_batch = data_batch.to(model.device), label_batch.to(model.device)
+                # one_hot_label_batch = one_hot_encode(label_batch)
+                output = model(data_batch)
+                loss = criterion(output, data_batch)
+                agg_test_loss += loss.item()
+
+                _, predicted = torch.max(output, 1)
+                correct_test += (predicted == data_batch).sum().item()
+                total_test += data_batch.size(0)
+        test_loss.append(agg_test_loss / len(test_loader))
+
+        print(f'Epoch {epoch + 1}/{NUM_EPOCHS}, Train Loss: {train_loss[-1]:.4f}, Test Loss: {test_loss[-1]:.4f}')
+
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(train_loss, label='Train Loss')
+    plt.plot(test_loss, label='Test Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Training and Test Loss Curves')
+    plt.legend()
+    plt.savefig('loss_plot.png')
+    plt.show()
+
+    # plt.subplot(1, 2, 2)
+    # # plt.plot(train_accuracy, label='Train Accuracy')
+    # plt.plot(test_accuracy, label='Test Accuracy')
+    # plt.xlabel('Epoch')
+    # plt.ylabel('Accuracy (%)')
+    # plt.title('Training and Test Accuracy Curves')
+    # plt.legend()
+    # plt.savefig('accuracy_plot.png')
+    # plt.show()
+    # print(train_accuracy)
+    return model.encoder
 
 def Q1(train_loader):
     model = Autoencoder.Autoencoder()
@@ -46,14 +120,16 @@ def Q1(train_loader):
 
     return model
 
-def Q2(train_loader, test_loader):
+def Q2(train_loader, test_loader, trained_encoder= None):
     model = Classifier.Classifier()
+    if trained_encoder is not None:
+        model.encoder = trained_encoder
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     train_loss, test_loss = [],[]
-    train_accuracy, test_accuracy = [], []
+    test_accuracy = []
     for epoch in range(NUM_EPOCHS):
-        model.train()
+        model.train() #why do you take it out from the next loop? we dont want train on every batch?
         agg_train_loss = 0
         correct_train = 0
         total_train = 0
@@ -74,6 +150,7 @@ def Q2(train_loader, test_loader):
             total_train += label_batch.size(0)
 
         train_loss.append(agg_train_loss / len(train_loader))
+        # train_accuracy.append(100 * correct_train / total_train)
 
 
         model.eval()
@@ -109,7 +186,7 @@ def Q2(train_loader, test_loader):
     plt.show()
 
     plt.subplot(1, 2, 2)
-    plt.plot(train_accuracy, label='Train Accuracy')
+    # plt.plot(train_accuracy, label='Train Accuracy')
     plt.plot(test_accuracy, label='Test Accuracy')
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy (%)')
@@ -117,6 +194,7 @@ def Q2(train_loader, test_loader):
     plt.legend()
     plt.savefig('accuracy_plot.png')
     plt.show()
+    # print(train_accuracy)
     return model.encoder
 
 
@@ -202,13 +280,44 @@ def one_hot_encode(batch_labels, num_classes=10):
 
     return torch.tensor(one_hot_vectors, dtype=torch.float32)
 
+def Q4(test_loader, trained_encoder= None):
+    indices = torch.arange(100)
+    # BATCH_SIZE = 256
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+    train_dataset = datasets.MNIST(root='./data', train=True,
+                                   transform=transform, download=True)
+
+    train_loader_CLS = Subset(train_dataset, indices)
+
+    train_loader = DataLoader(train_loader_CLS, batch_size=BATCH_SIZE,
+                              shuffle=True)
+
+
+    # train_loader_CLS = torch.utils.data.DataLoader(train_loader_CLS,
+    #                                                batch_size=BATCH_SIZE,
+    #                                                shuffle=True, num_workers=0)
+    Q2(train_loader, test_loader, trained_encoder)
+
+def Q5(test_loader):
+    # Save the pre-trained encoder
+    # torch.save(encoder.state_dict(), 'pretrained_encoder.pth') #todo enter to any function in Q1
+    encoder = Autoencoder.Autoencoder()
+    encoder.load_state_dict(torch.load(PATH_TO_SAVED_AE))
+    Q4(test_loader, encoder)
+
 
 if __name__ == '__main__':
     train_loader, test_loader, test_data = data_loader()
-    AE= Q1(train_loader)
-    pretrained_encoder = Q2(train_loader, test_loader)
+    Q1_S(train_loader, test_loader)
+    # Q2(train_loader, test_loader)
+    # AE= Q1(train_loader)
+    # pretrained_encoder = Q2(train_loader, test_loader)
     #
     # AE = Autoencoder.Autoencoder()
     # pretrained_encoder = Autoencoder.Encoder()
 
-    Q3(train_loader, test_data, pretrained_encoder, AE)
+    # Q3(train_loader, test_data, pretrained_encoder, AE)
+
+    # Q4(test_loader)
